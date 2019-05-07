1f6016585ef0c69d11af6b57691184f26bdce77d
zengchencn <zengchen@ruc.edu.cn>
2019-05-07 02:04:56 +0000

    first init.

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Eagle from 'eagle.js'
+import 'eagle.js/dist/eagle.css'
+import 'animate.css'
+
+Vue.use(Eagle)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Presentation.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  h1 Hello, World!
+</template>
+<script>
+export default {
+  methods: {
+    updateSlides: function () {
+      this.currentSlideIndex = +this.$route.params.slide
+      this.$nextTick(() => {
+        this.step = +this.$route.params.step
+      })
+    },
+    updateURL: function () {
+      this.$router.push(`/${this.currentSlideIndex}/${this.step}`)
+    }
+  },
+  watch: {
+    $route: 'updateSlides',
+    step: 'updateURL',
+    currentSlideIndex: 'updateURL'
+  }
+}
+</script>
